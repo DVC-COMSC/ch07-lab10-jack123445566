d5be3c69eab5d6b511302c629e785ea1541da9b3
Jack <jackcshin@gmail.com>
2022-11-01 11:43:59 -0700

    1st commit

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
-# ******************************
-# Make your Code
-# ******************************
+Product = list(map(int, input().split( )))
+print(Product)
+for i in range(len(Product)):
+    smallest = 0
+    for j in range(0,len(Product)-i):
+        crrnt_index = i+j
+        if smallest == 0:
+            smalles = Product[crrnt_index]
+            ind = crrnt_index
+        if Product[crrnt_index] < smallest:
+            smallest = Product[crrnt_index]
+            ind = crrnt_index
+    Product[i],Product[ind] = Product[ind],Product[i]
+    print(Product)
+
